79301f165ffbd580ae7ed8945b7e43bb2c9be263
Jordielson <jordielsonemanuel@gmail.com>
2022-08-09 08:41:07 -0300

    preview video stream

--- camera_pipeline/src/main/java/com/camerapipeline/camera_pipeline/controller/pipeline/PipelineController.java
+++ camera_pipeline/src/main/java/com/camerapipeline/camera_pipeline/controller/pipeline/PipelineController.java
@@ -6,6 +6,12 @@ import com.camerapipeline.camera_pipeline.mapper.pipeline.PipelineMapper;
 import com.camerapipeline.camera_pipeline.model.pipeline.Pipeline;
 import com.camerapipeline.camera_pipeline.services.pipeline.PipelineService;
 
+import java.security.Principal;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PipelineController extends ControllerAbstract<Pipeline, PipelineDTO, Integer> {
     public PipelineController(PipelineService service, PipelineMapper mapper) {
         super(service, mapper);
+    }
+
+    @PostMapping("/preview/{id}")
+    public ResponseEntity<String> execute(@PathVariable("id") Integer id, Principal principal) {
+        return new ResponseEntity<String>("rtsp://rtsp.stream/pattern", HttpStatus.OK);
     }
 }
