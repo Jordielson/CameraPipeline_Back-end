65b58d30d3e8cbdcd9ee82064301c31e86a3cdca
Jordielson <jordielsonemanuel@gmail.com>
2022-10-09 10:29:09 -0300

    change http method to generate camera

--- src/main/java/com/camerapipeline/camera_pipeline/presentation/controller/input/camera/CameraController.java
+++ src/main/java/com/camerapipeline/camera_pipeline/presentation/controller/input/camera/CameraController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -317,7 +318,7 @@ public class CameraController extends ControllerAbstract<Camera, CameraDTO, UUID
             schema = @Schema( implementation = ExceptionMessage.class))
         ),
     })
-    @GetMapping("/generateCamera")
+    @PostMapping("/generateCamera")
 	public ResponseEntity<CameraDTO> generateCamera(
         Principal principal,
         @Parameter(
